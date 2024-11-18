4db296b342aef374e1294516e254ca2291fe58b2
2669153076 <2669153076@qq.com>
2024-11-18 22:58:14 +0800

    11/18

--- Assets/FrameWork/ProjectBase/AStar/AStarMgr.cs
+++ Assets/FrameWork/ProjectBase/AStar/AStarMgr.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,8 +8,8 @@ public class AStarMgr : BaseManager<AStarMgr>
     public int mapH;    //地图高
 
     public AStarNode[,] starNodes;  //地图所有的格子对象容器
-    private List<AStarNode> openList;   //开启列表
-    private List<AStarNode> closeList;  //关闭列表
+    private List<AStarNode> openList = new List<AStarNode>();   //开启列表
+    private List<AStarNode> closeList = new List<AStarNode>();  //关闭列表
 
     /// <summary>
     /// 初始化地图信息
@@ -22,12 +22,12 @@ public class AStarMgr : BaseManager<AStarMgr>
         this.mapH = mapH;
         starNodes = new AStarNode[mapW, mapH];  //初始化容器
         //根据宽高 创建格子 
-        for (int i = 0;i<mapW;i++)
+        for (int i = 0; i < mapW; i++)
         {
-            for(int j = 0; j < mapH; j++)
+            for (int j = 0; j < mapH; j++)
             {
-                AStarNode node = new AStarNode(i,j,Random.Range(0,100)<20?E_NodeType.Stop:E_NodeType.Walk);
-                starNodes[i,j] = node;
+                AStarNode node = new AStarNode(i, j, Random.Range(0, 100) < 20 ? E_NodeType.Stop : E_NodeType.Walk);
+                starNodes[i, j] = node;
             }
         }
     }
@@ -38,20 +38,20 @@ public class AStarMgr : BaseManager<AStarMgr>
     /// <param name="startPos"></param>
     /// <param name="endPos"></param>
     /// <returns></returns>
-    public List<AStarNode> FindPath(Vector2 startPos,Vector2 endPos)
+    public List<AStarNode> FindPath(Vector2 startPos, Vector2 endPos)
     {
         //如果开始的点或结束的点在地图之外
-        if (startPos.x < 0 || startPos.x >= mapW || 
-            startPos.y < 0 || startPos.y >= mapH || 
-            endPos.x < 0 || endPos.x >= mapW || 
+        if (startPos.x < 0 || startPos.x >= mapW ||
+            startPos.y < 0 || startPos.y >= mapH ||
+            endPos.x < 0 || endPos.x >= mapW ||
             endPos.y < 0 || endPos.y >= mapH)
         {
             return null;
         }
-        AStarNode startNode = starNodes[(int)startPos.x,(int)startPos.y];
-        AStarNode endNode  = starNodes[(int)endPos.x,(int)endPos.y];
+        AStarNode startNode = starNodes[(int)startPos.x, (int)startPos.y];
+        AStarNode endNode = starNodes[(int)endPos.x, (int)endPos.y];
         //如果开始的点或结束的点的类型是障碍物
-        if(startNode.type == E_NodeType.Stop||endNode.type == E_NodeType.Stop)
+        if (startNode.type == E_NodeType.Stop || endNode.type == E_NodeType.Stop)
         {
             return null;
         }
@@ -60,32 +60,57 @@ public class AStarMgr : BaseManager<AStarMgr>
         //清空关闭和开启列表
         closeList.Clear();
         openList.Clear();
+        startNode.Clear();//清空开始节点
         //开始的点放入关闭列表中
-        startNode.Clear();
         closeList.Add(startNode);
 
-        //左上
-        FindNearlyNodeToOpenList(startNode.x - 1, startNode.y - 1, 1.4f, startNode, endNode);
-        //上
-        FindNearlyNodeToOpenList(startNode.x, startNode.y - 1, 1.4f, startNode, endNode);
-        //右上
-        FindNearlyNodeToOpenList(startNode.x + 1, startNode.y - 1, 1.4f, startNode, endNode);
-        //左
-        FindNearlyNodeToOpenList(startNode.x - 1, startNode.y, 1.4f, startNode, endNode);
-        //右
-        FindNearlyNodeToOpenList(startNode.x + 1, startNode.y, 1.4f, startNode, endNode);
-        //左下
-        FindNearlyNodeToOpenList(startNode.x - 1, startNode.y + 1, 1.4f, startNode, endNode);
-        //下
-        FindNearlyNodeToOpenList(startNode.x, startNode.y + 1, 1.4f, startNode, endNode);
-        //右下
-        FindNearlyNodeToOpenList(startNode.x + 1, startNode.y + 1, 1.4f, startNode, endNode);
+        while (true)
+        {   
+            //左上
+            FindNearlyNodeToOpenList(startNode.x - 1, startNode.y - 1, 1.4f, startNode, endNode);
+            //上
+            FindNearlyNodeToOpenList(startNode.x, startNode.y - 1, 1, startNode, endNode);
+            //右上
+            FindNearlyNodeToOpenList(startNode.x + 1, startNode.y - 1, 1.4f, startNode, endNode);
+            //左
+            FindNearlyNodeToOpenList(startNode.x - 1, startNode.y, 1, startNode, endNode);
+            //右
+            FindNearlyNodeToOpenList(startNode.x + 1, startNode.y, 1, startNode, endNode);
+            //左下
+            FindNearlyNodeToOpenList(startNode.x - 1, startNode.y + 1, 1.4f, startNode, endNode);
+            //下
+            FindNearlyNodeToOpenList(startNode.x, startNode.y + 1, 1, startNode, endNode);
+            //右下
+            FindNearlyNodeToOpenList(startNode.x + 1, startNode.y + 1, 1.4f, startNode, endNode);
 
-        openList.Sort(SortOpenList);
+            //死路
+            if (openList.Count == 0)
+            {
+                Debug.Log("死路");
+                return null;
+            }
 
+            openList.Sort(SortOpenList);
 
+            closeList.Add(openList[0]); //将最短路径节点添加到关闭列表中
+            startNode = openList[0];    //将该最短路径节点设置为新的开始点
+            openList.RemoveAt(0);   //开启列表中删除该最短路径节点
 
-        return null;
+            if (startNode == endNode)
+            {
+                //找到了路径
+                List<AStarNode> path = new List<AStarNode>();
+                path.Add(endNode);
+                while (endNode.father != null)
+                {
+                    path.Add(endNode.father);
+                    endNode = endNode.father;
+                }
+
+                path.Reverse(); //列表翻转
+                return path;
+            }
+        }
     }
 
     /// <summary>
@@ -98,12 +123,12 @@ public class AStarMgr : BaseManager<AStarMgr>
     /// <param name="endNode">终点</param>
     private void FindNearlyNodeToOpenList(int x, int y, float g, AStarNode father, AStarNode endNode)
     {
-        if (x < 0 || x > mapW || y < 0 || y > mapH)
+        if (x < 0 || x >= mapW || y < 0 || y >= mapH)
         {
             return;
         }
 
-        AStarNode node = starNodes[x,y];    //邻近的点
+        AStarNode node = starNodes[x, y];    //邻近的点
 
         //如果为空，类型为障碍，在关闭、开启列表中
         if (node == null || node.type == E_NodeType.Stop || closeList.Contains(node) || openList.Contains(node))
@@ -114,9 +139,9 @@ public class AStarMgr : BaseManager<AStarMgr>
         //计算总消耗 f=g+h
         node.father = father;
         node.g = father.g + g;  //离起点的距离 = 父物体离起点的距离 + 我当前离父物体的距离
-        node.h  = Mathf.Abs(endNode.x-node.x)+Mathf.Abs(endNode.y-node.y);
+        node.h = Mathf.Abs(endNode.x - node.x) + Mathf.Abs(endNode.y - node.y);
         node.f = node.g + node.h;
-        
+
 
         openList.Add(node);
     }
@@ -130,9 +155,11 @@ public class AStarMgr : BaseManager<AStarMgr>
     private int SortOpenList(AStarNode a, AStarNode b)
     {
         if (a.f > b.f)
-            return -1;
-        else
             return 1;
+        else if (a.f == b.f)
+            return 1;
+        else
+            return -1;
     }
 
 }
---
+++ Assets/Scripts/TestAStar.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAStar : MonoBehaviour
+{
+    //格子左上角初始坐标
+    public int beginX = -3;
+    public int beginY = 5;
+    //格子间距
+    public int offsetX = 2;
+    public int offsetY = -2;
+    //地图宽高
+    public int mapW = 5;
+    public int mapH = 5;
+
+    private Dictionary<string,GameObject> cubeDir = new Dictionary<string,GameObject>();
+    private Vector2 beginPos = Vector2.right * -1;  //开始点 给一个负值
+    private List<AStarNode> list;   //最短路径列表
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AStarMgr.GetInstance().InitMapInfo(mapW, mapH);
+        
+
+        for (int i = 0; i < mapW; i++)
+        {
+            for (int j = 0; j < mapH; j++)
+            {
+                GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                obj.transform.position = new Vector3(beginX + i * offsetX, beginY + j * offsetY, 0);
+
+                obj.name = i + "_" + j;
+                cubeDir.Add(obj.name, obj);
+
+                AStarNode node = AStarMgr.GetInstance().starNodes[i, j];
+                if(node.type == E_NodeType.Stop)
+                {
+                    obj.GetComponent<MeshRenderer>().material.color = Color.red;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit, 1000))
+            {
+                if(beginPos == Vector2.right * -1)
+                {
+                    //清理上次的路径
+                    if (list != null)
+                    {
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            cubeDir[list[i].x + "_" + list[i].y].GetComponent<MeshRenderer>().material.color = Color.white;
+                        }
+                    }
+
+                    //得到起点
+                    string[] strs = raycastHit.collider.gameObject.name.Split('_');
+                    beginPos = new Vector2(int.Parse(strs[0]), int.Parse(strs[1]));
+                    raycastHit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                }
+                else
+                {
+                    //得到终点
+                    string[] strs = raycastHit.collider.gameObject.name.Split('_');
+                    Vector2 endPos = new Vector2(int.Parse(strs[0]), int.Parse(strs[1]));
+                    //raycastHit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+
+                    list = AStarMgr.GetInstance().FindPath(beginPos, endPos); 
+                    cubeDir[(int)beginPos.x + "_" + (int)beginPos.y].GetComponent<MeshRenderer>().material.color = Color.white;
+                    if (list != null)
+                    {
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            cubeDir[list[i].x + "_" + list[i].y].GetComponent<MeshRenderer>().material.color = Color.green;
+                        }
+                    }
+                    beginPos = Vector2.right * -1;  //清除开始的点
+                }
+            }
+        }
+    }
+}
